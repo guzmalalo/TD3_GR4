5ae3cca1cf892db7a5391484b056eac72a5919d4
Eduardo Guzman <eduardo.guzman@innovamics.com>
2023-10-12 13:08:53 +0200

    Corrections exo 3

--- exo3.c
+++ exo3.c
@@ -7,13 +7,21 @@ int main(void){
     printf("Donnez un entier :");
     scanf("%d", &n);
 
-    printf("Le numero %d est mutiple de 3  %c \n", n, n%3==0?'O':'N');
+    printf("Le numéro %d est multiple de 3 ? : %c \n", n, n%3==0?'O':'N');
+    printf("Le numéro %d est superieur à 10 ? : %c \n", n, n>10?'O':'N');
 
+
+    // OU
+    /*
     if(n>10){
-        printf("Le numero %d est sup à 10",n);
+        printf("Le numéro %d est sup à 10",n);
     }else{
-        printf("Le numero %d n'est pas sup à 10",n);
+        printf("Le numéro %d n'est pas sup à 10",n);
     }
+    */
+
+    // condition composée
+    printf("Le numero %d est multiple de 3 ET sup à 10 ? : %c ", n, (n%a==0 && n>10)? 'O':'N');
 
     return 0;
 }
